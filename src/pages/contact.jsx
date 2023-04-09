a137ecf6ed9eb262352c352425822bafc5c0abb5
leshamaybe <vvaleksey@icloud.com>
2023-04-09 16:49:05 +0300

    added a form to the contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import MainContainer from '@/components/container/MainContainer';
+import ContactForm from '@/components/contact/ContactForm';
 
 const contact = () => {
     return (
         <MainContainer>
-            contact!
+            <ContactForm />
         </MainContainer>
     );
 };
