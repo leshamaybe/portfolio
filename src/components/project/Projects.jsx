81d7dad570f7ff8d1fd365e9f07ca9c05aeb8889
leshamaybe <vvaleksey@icloud.com>
2023-04-12 15:41:33 +0300

    change works page

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 import s from './project.module.scss';
 import Button from './button/Button';
 
-const Projects = ({ id, name, desc, tools, src, link }) => {
+const Project = ({ id, name, desc, tools, src, link }) => {
+    const variants = {
+        scale: {
+            scale: [null, 0.9, 0.9],
+            transition: {
+                duration: 0.4,
+            },
+        },
+    };
+
     return (
         <div className={s.box}>
-            <motion.div className={s['img-box']}>
-                <Image src={src} />
+            <motion.div variants={variants} whileHover="scale" className={s['img-box']}>
+                <Link href={link}>
+                    <Image src={src} />
+                </Link>
             </motion.div>
 
             <div className={s['text-box']}>
-                <h2>App, Messenger</h2>
+                <h2>Featured Project</h2>
                 <h1>{name}</h1>
-
+                <ul>
+                    {tools.map((tool) => {
+                        return <li key={tool}>{tool}</li>;
+                    })}
+                </ul>
                 <Button classname={s['btn-work']} link={link} />
             </div>
         </div>
     );
 };
 
-export default Projects;
+export default Project;
--- src/components/screens/works/WorkExp.jsx
+++ src/components/screens/works/WorkExp.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Projects from '../../project/Projects';
+import Project from '../../project/Projects';
 import weather from '../../../assets/works/weather.png';
 import chat from '@/assets/works/chat-group2.png';
 import whales from '../../../assets/works//whales.png';
+import s from './work.module.scss';
 
 const projectItems = [
     {
         id: 1,
         name: 'Full realtime Chat Application',
-        desc: "App, Messenger, chat",
-        tools: ['React', 'Hooks', 'Express', 'Socket.io', 'MongoDB', 'Axios', 'JWT-token'],
+        desc: 'The Messenger app is a real-time chat application built with React, JavaScript, Socket.io, Express, and MongoDB. It allows users to communicate through instant messaging, and provides a dynamic and responsive interface for sending and receiving messages.',
+        tags: 'App, Messenger, chat',
+        tools: ['React', 'Hooks', 'Express', 'Socket.io', 'MongoDB'],
         img: chat,
         link: 'https://github.com/leshamaybe/react-express-socket-chat-app',
     },
@@ -17,6 +19,7 @@ const projectItems = [
         id: 2,
         name: 'Weather app on the OpenWeatherMap API',
         desc: 'A weather application built with React that displays current weather conditions for a selected location. Uses OpenWeatherMap API for weather data.',
+        tags: 'App, Weather, API',
         tools: ['React', 'Hooks', 'Axios'],
         img: weather,
         link: 'https://github.com/leshamaybe/react-weather-app',
@@ -25,6 +28,7 @@ const projectItems = [
         id: 3,
         name: 'A ReactJS based landing page',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos optio asperiores aliquid in. Et dolor nobis, ad facere asperiores dicta cum numquam consequuntur tempora corporis?',
+        tags: 'Landing, Template',
         tools: ['React', 'Hooks', 'Redux', 'Express', 'MongoDB', 'Axios'],
         img: whales,
         link: '/',
@@ -34,9 +38,10 @@ const projectItems = [
 const WorkExp = () => {
     return (
         <>
+            <h1 className={s.title}>Projects</h1>
             {projectItems.map(({ id, name, desc, tools, img, link }) => {
                 return (
-                    <Projects
+                    <Project
                         id={id}
                         key={id}
                         name={name}
