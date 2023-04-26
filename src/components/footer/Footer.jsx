d9d3ab8ee595ee7592c7a7704256baeeab100a59
leshamaybe <vvaleksey@icloud.com>
2023-04-26 17:33:51 +0300

    change footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Social from '../social/Social';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import s from './footer.module.scss';
 
 const Footer = () => {
+    const { pathname } = useRouter();
+
     return (
         <footer className={s.footer}>
             <div className={s.container}>
-                <h2>© 2023 Aleksey Voronko</h2>
-                <Social />
+                <div className={s.empty}></div>
+                {pathname == '/resume' ? null : (
+                    <div className={s.top}>
+                        <h1>Have a project in mind or want to talk?</h1>
+                        <span>
+                            Say hey at
+                            <Link className={s.link} href={'mailto:vvaleksey@icloud.com'}>
+                                {' '}
+                                vvaleksey@icloud.com
+                            </Link>
+                        </span>
+                    </div>
+                )}
+                <div className={s.bottom}>
+                    <h2>© 2023 leshamaybe</h2>
+                    <Social />
+                </div>
             </div>
         </footer>
     );
