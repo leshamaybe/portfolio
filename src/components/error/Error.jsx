b30a5b1460aea8b443827d6906319275a5e2edea
leshamaybe <vvaleksey@icloud.com>
2023-05-07 23:15:05 +0300

    refactor: change error page

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -6,6 +6,7 @@ const Error404 = () => {
     return (
         <div className={s.error}>
             <h1>404</h1>
+            <h2>Ooops... There's nothing in here</h2>
             <Link href="/">&#8592;Back home page</Link>
         </div>
     );
