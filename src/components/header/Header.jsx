9bf28212f36cff6f3f75940fa4cedba1bbabbaa3
leshamaybe <vvaleksey@icloud.com>
2023-05-07 23:16:43 +0300

    perf: add dynamic import

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
+import useWindowSize from '@/hooks/useWindowSize';
 import Nav from '../navigation/Nav';
 import Logo from '../logo/Logo';
-import MarqueeBox from '../marquee/MarqueeBox';
 import s from './header.module.scss';
 
+const LazyMarquee = dynamic(() => import('../marquee/MarqueeBox'));
+
 const Header = () => {
+    const size = useWindowSize();
+
     return (
         <header className={s.header}>
             <div className={s.container}>
                 <Logo />
-                <MarqueeBox />
+                {size.width > 750 && <LazyMarquee />}
                 <Nav />
             </div>
         </header>
--- src/components/marquee/MarqueeBox.jsx
+++ src/components/marquee/MarqueeBox.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 import s from './marquee.module.scss';
-import useWindowSize from '@/hooks/useWindowSize';
 
 const MarqueeBox = () => {
-    const size = useWindowSize();
-
     return (
-        <>
-            {size.width > 750 && (
-                <Marquee gradientColor="[255, 255, 255]" speed="150" className={s.marquee}>
-                    <h1>AVAILABLE FOR WORK</h1>
-                </Marquee>
-            )}
-        </>
+        <Marquee gradientColor="[255, 255, 255]" speed="150" className={s.marquee}>
+            <h1>AVAILABLE FOR WORK</h1>
+        </Marquee>
     );
 };
 
