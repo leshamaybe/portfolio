493e92de8f1badb461abc386c03aa97663e7a5b4
leshamaybe <vvaleksey@icloud.com>
2023-06-07 23:56:52 +0300

    test: add jest setup/config

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+/** @type {import('jest').Config} */
+const config = {
+    collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**'],
+    extensionsToTreatAsEsm: ['.jsx'],
+    moduleNameMapper: {
+        '^@/components/(.*)$': '<rootDir>/components/$1',
+        '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    },
+    testEnvironment: 'jest-environment-jsdom',
+};
+
+export default createJestConfig(config);
